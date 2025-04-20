da59431a11347594a671af76d8fb8cc2c7bc94e9
Elkin Kirill <kirillv.2016@mail.ru>
2025-04-20 07:05:26 +0300

    frontend #2

--- web_service/main/decorators.py
+++ web_service/main/decorators.py
@@ -5,7 +5,7 @@ from django.shortcuts import redirect
 def login_required(view_func):
     def _wrapped_view(request, *args, **kwargs):
         if "user_id" not in request.session:
-            return redirect("/login/")
+            return redirect("authorization")
         return view_func(request, *args, **kwargs)
 
     return _wrapped_view
